fe2595c7271a5952241782687522830035977d7f
Jonasdad <Jonasdad@kth.se>
2023-02-17 09:41:54 +0100

    Almost finished

--- MonteCarlo.ex
+++ MonteCarlo.ex
@@ -8,13 +8,18 @@ defmodule MonteCarlo do
     rounds(k, j, 0, r, 0)
   end
   def rounds(0, _, t, _, a) do
-    t/a
+    4*a/t
   end
+  #a = antalet träffat innan för arch
+  #j = antalet kast
+  #t = antalet rundor
+  #k
   def rounds(k,j,t,r,a) do
     a = round(j,r,a)
     t = t+j
-    pi = a / t
-    :io.format("pi", [pi, pi - :math.pi()])
+    IO.puts("#{4*a} / #{t} = #{4*a/t}")
+    pi = (4*a)/t
+    :io.format("~14.10f\n", [pi, pi - :math.pi()])
     rounds(k-1, j, t, r, a)
   end
 
